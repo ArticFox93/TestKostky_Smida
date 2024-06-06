af1e49a37f0cf26005b3f8f2023dfa98086818de
ArticFox93 <pavelsmida16@seznam.cz>
2024-06-06 14:10:23 +0200

    hod kostkou pomoci random

---
+++ TestKostky_Smida/Kosticka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKostky_Smida
+{
+    public class Kosticka
+    {
+        private static Random random = new Random();
+        public int value { get; set; }
+
+
+        public void Hod()
+        {
+            random.Next(1, 7);
+        }
+
+        public Kosticka()
+        {
+            Hod();
+        }
+    }
+}
